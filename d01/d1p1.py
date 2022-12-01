4295f591671b9963dd1a4c6bd18b4d51e27c54f2
Javier Abadía <javi.abadia@gmail.com>
2022-12-01 09:40:19 +0100

    d01 completed

---
+++ d01/d1p1.py
@@ -0,0 +1,37 @@
+from utils.test_case import TestCase
+from d1_input import INPUT
+
+TEST_CASES = [
+    TestCase("""
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+""", 24000),
+]
+
+
+def solve(input):
+    elves_calories = input.strip().split('\n\n')
+    return max(
+        sum(int(calories) for calories in elf_calories.split('\n'))
+        for elf_calories in elves_calories
+    )
+
+
+if __name__ == '__main__':
+    for case in TEST_CASES:
+        result = solve(case.case)
+        case.check(result)
+
+    print(solve(INPUT))
