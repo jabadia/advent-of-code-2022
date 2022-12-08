9d829aa7d8c1da57d881e9aaecefeb6b836524cf
Javier Abadía <javi.abadia@gmail.com>
2022-12-08 23:19:23 +0100

    day 4 completed

---
+++ d04/d4p1.py
@@ -0,0 +1,37 @@
+from utils.fetch_input import fetch_input
+from utils.test_case import TestCase
+
+TEST_CASES = [
+    TestCase("""
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+""", 2),
+]
+
+
+def is_fully_contained(r0, r1, p0, p1):
+    return r0 <= p0 and p1 <= r1
+
+
+def solve(input):
+    count = 0
+    for pair in input.strip().split('\n'):
+        elf1, elf2 = pair.split(',')
+        r0, r1 = map(int, elf1.split('-'))
+        p0, p1 = map(int, elf2.split('-'))
+        if is_fully_contained(r0,r1,p0,p1) or is_fully_contained(p0,p1,r0,r1):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    input = fetch_input()
+    for case in TEST_CASES:
+        result = solve(case.case)
+        case.check(result)
+
+    print(solve(input))
