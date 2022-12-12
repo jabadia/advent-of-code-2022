bbe2614cd4c54e936a5e56720ce953068b276204
Javier Abadía <javi.abadia@gmail.com>
2022-12-12 21:09:51 +0100

    day 10 completed

---
+++ d10/d10p1.py
@@ -0,0 +1,191 @@
+from utils.fetch_input import fetch_input
+from utils.test_case import TestCase
+
+TEST_CASES = [
+    TestCase("""
+noop
+addx 3
+addx -5
+""", 0),
+    TestCase("""
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+""", 13140)
+]
+
+
+def solve(input):
+    x = 1
+    cycle = 0
+    signal_strength = 0
+    for instruction in input.strip().split('\n'):
+        if instruction == 'noop':
+            cycle += 1
+            if (cycle + 20) % 40 == 0:
+                signal_strength += cycle * x
+        else:
+            _, value = instruction.split(' ')
+            assert _ == 'addx'
+            value = int(value)
+            cycle += 1
+            if (cycle + 20) % 40 == 0:
+                signal_strength += cycle * x
+            cycle += 1
+            if (cycle + 20) % 40 == 0:
+                signal_strength += cycle * x
+            x += value
+        # print(cycle, x)
+    return signal_strength
+
+
+if __name__ == '__main__':
+    input = fetch_input()
+    for case in TEST_CASES:
+        result = solve(case.case)
+        case.check(result)
+
+    print(solve(input))
