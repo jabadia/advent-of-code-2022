6c991e983d75ca6928849feaa2248633909c6b0b
Javier Abadía <javi.abadia@gmail.com>
2022-12-13 20:02:26 +0100

    day 13 completed

---
+++ d13/d13p1.py
@@ -0,0 +1,87 @@
+from utils.fetch_input import fetch_input
+from utils.test_case import TestCase
+
+TEST_CASES = [
+    TestCase("""
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+""", 13)
+]
+
+
+# def fix_types(first, second):
+#     if isinstance(first, list) != isinstance(second, list):
+#         if isinstance(first, list):
+#             return first, [second]
+#         else:
+#             return [first], second
+#     elif isinstance(first, list):
+#         new_first = []
+#         new_second = []
+#         for item1, item2 in zip(first, second):
+#             item1, item2 = fix_types(item1, item2)
+#             new_first.append(item1)
+#             new_second.append(item2)
+#         return new_first, new_second
+#     else:
+#         return first, second
+
+def compare(first, second):
+    if isinstance(first, int) and isinstance(second, int):
+        return first - second
+    else:
+        if isinstance(first, int) != isinstance(second, int):
+            if isinstance(first, int):
+                first = [first]
+            else:
+                second = [second]
+        # both are lists now
+        for item1, item2 in zip(first, second):
+            cmp = compare(item1, item2)
+            if cmp == 0:
+                continue
+            return cmp
+        return len(first) - len(second)
+
+
+def solve(input):
+    result = 0
+    for index, pair in enumerate(input.strip().split('\n\n')):
+        first, second = pair.split('\n')
+        first = eval(first)
+        second = eval(second)
+        # first, second = fix_types(first, second)
+        if compare(first, second) < 0:
+            result += (index + 1)
+
+    return result
+
+
+if __name__ == '__main__':
+    input = fetch_input()
+    for case in TEST_CASES:
+        result = solve(case.case)
+        case.check(result)
+
+    print(solve(input))
---
+++ d13/d13p2.py
@@ -0,0 +1,67 @@
+from utils.fetch_input import fetch_input
+from utils.test_case import TestCase
+import functools
+
+TEST_CASES = [
+    TestCase("""
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+""", 140)
+]
+
+
+def compare(first, second):
+    if isinstance(first, int) and isinstance(second, int):
+        return first - second
+    else:
+        if isinstance(first, int) != isinstance(second, int):
+            if isinstance(first, int):
+                first = [first]
+            else:
+                second = [second]
+        # both are lists now
+        for item1, item2 in zip(first, second):
+            cmp = compare(item1, item2)
+            if cmp == 0:
+                continue
+            return cmp
+        return len(first) - len(second)
+
+
+def solve(input):
+    packets = input.strip().split('\n')
+    packets = [eval(packet) for packet in packets if packet] + [[[2]], [[6]]]
+    packets = sorted(packets, key=functools.cmp_to_key(compare))
+    marker1 = 1 + packets.index([[2]])
+    marker2 = 1 + packets.index([[6]])
+    return marker1 * marker2
+
+
+if __name__ == '__main__':
+    input = fetch_input()
+    for case in TEST_CASES:
+        result = solve(case.case)
+        case.check(result)
+
+    print(solve(input))
